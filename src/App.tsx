1e5128feec87f0bb524dd5889b7310088d4f7770
TatsuyaUeno <tatsuya_soccer222@icloud.com>
2024-04-27 15:27:02 +0900

    “pushコマンドの確認”

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './components/Login';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' Component={Login} />
+        <Route path='/chat' Component={Chat} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+
+
+function Login() {
+    return (
+      <h1>
+        ログイン画面
+      </h1>
+    );
+  }
+  
+  export default Login;
